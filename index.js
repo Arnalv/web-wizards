68c6876eb1f452d74047c6f2523eaf1b8d8eef74
Adit Rajaram <aditrajaram@unknown225c23a6ec52.home>
2025-06-01 11:43:05 -0500

    Course Resources

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const port = 6500;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.render("Index.ejs"));
+app.get("/course-resources", (req, res) => res.render("courseParts.ejs"))
 
 app.listen(port, console.log(`listening on port ${port}`));
