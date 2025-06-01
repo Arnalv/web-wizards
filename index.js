006d1e4ea1484facbc1d2d8a753f6a829ad761d7
Arnalv <arnavarora751@gmail.com>
2025-06-01 13:13:17 -0500

    add about us

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => res.render("Index.ejs"));
 app.get("/course-resources", (req, res) => res.render("courseParts.ejs"))
+app.get("/aboutus", (req, res) => res.render("aboutus.ejs"))
 
 app.listen(port, console.log(`listening on port ${port}`));
